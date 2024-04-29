c191e5953ff8c460de230a2b1e66f0c45f4f271b
TheCometIsNotComing <ramunasrimidis@gmail.com>
2024-04-29 11:54:22 +0300

    code changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 public static void main(String[] args) {
-System.out.println("Hello, world!");
+System.out.println("Hello, " + "world!");
+int l = 0;
+int a = l + l;
+   System.out.println("Ploshad square " + l + " = " + a);
 
 }
 }
